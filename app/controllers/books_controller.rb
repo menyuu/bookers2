8be6f23f9f259d24785a1b52f076351a2db62e1e
menyuu <101458830+menyuu@users.noreply.github.com>
2022-06-03 18:29:59 +0000

    edit,destroyリンクの追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -21,6 +21,11 @@ class BooksController < ApplicationController
   def edit
   end
 
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
 
   private
 
