969fbdef2ec2e9cc139056001f60f864adc339e6
menyuu <101458830+menyuu@users.noreply.github.com>
2022-06-04 03:04:23 +0000

    validationの追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  
+
   before_action :correct_user, only: [:edit, :update]
 
   def index
@@ -20,9 +20,12 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-    @user.update(user_params)
-    flash[:notice] = "You have updated user successfully."
-    redirect_to user_path(@user.id)
+    if @user.update(user_params)
+      flash[:notice] = "You have updated user successfully."
+      redirect_to user_path(@user.id)
+    else
+      render :edit
+    end
   end
 
 
