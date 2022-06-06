d66f1d37025e5620848d73413ce9413af44cbf3e
menyuu <101458830+menyuu@users.noreply.github.com>
2022-06-06 08:42:24 +0000

    favorite機能更新

--- app/models/book.rb
+++ app/models/book.rb
@@ -6,5 +6,9 @@ class Book < ApplicationRecord
   belongs_to :user
   
   has_many :favorites, dependent: :destroy
+  
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+  end
 
 end
