fd4e279815b42f76e03545029da7a112a1c1b57d
menyuu <101458830+menyuu@users.noreply.github.com>
2022-06-06 12:32:17 +0000

    comment機能の更新

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,11 +1,9 @@
 class BookCommentsController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
     comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
     comment.save
-    redirect_to book_path(book)
+    redirect_to book_path(params[:book_id])
   end
 
   def destroy
@@ -17,7 +15,7 @@ class BookCommentsController < ApplicationController
   private
 
   def book_comment_params
-    params.require(:book_comment).permit(:comment)
+    params.require(:book_comment).permit(:comment, :book_id)
   end
 
 end
