e241995cb1045c4e3ae597e7fb20f8042aeb13b6
menyuu <101458830+menyuu@users.noreply.github.com>
2022-06-07 03:21:21 +0000

    relationships_controllerの変更

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,5 +1,7 @@
 class RelationshipsController < ApplicationController
 
+  before_action :forbid_follow, only: [:create]
+
   def create
     current_user.follow(params[:user_id])
     redirect_to request.referer
@@ -18,5 +20,15 @@ class RelationshipsController < ApplicationController
   def followers
     user = User.find(params[:user_id])
     @users = user.followers
+  end
+
+
+  private
+
+  def forbid_follow
+    user = User.find(params[:user_id])
+    unless current_user != user
+      redirect_to request.referer
+    end
   end
 end
