0026daa17a579d8e23a44096a9ae54bc05004283
menyuu <101458830+menyuu@users.noreply.github.com>
2022-06-07 13:26:32 +0000

    いいね機能の非同期通信化

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,14 +1,16 @@
 class FavoritesController < ApplicationController
 
   def create
-    favorite = Favorite.new(user_id: current_user.id, book_id: params[:book_id])
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.new(book_id: @book.id)
     favorite.save
-    redirect_to request.referer
+    # redirect_to request.referer
   end
 
   def destroy
-    favorite = Favorite.find_by(user_id: current_user.id, book_id: params[:book_id])
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.find_by(book_id: @book.id)
     favorite.destroy
-    redirect_to request.referer
+    # redirect_to request.referer
   end
 end
