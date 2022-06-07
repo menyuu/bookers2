979b39add58ce8afd57075fe3761177fdfc9fa5e
menyuu <101458830+menyuu@users.noreply.github.com>
2022-06-07 02:38:24 +0000

    user_modelにfollowに関してのメソッドを記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,11 +12,11 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
 
   has_many :book_comments, dependent: :destroy
-  
+
   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   has_many :followings, through: :relationships, source: :followed
-  has_many :followers, through: :reverse_of_relationships, source: :follo
+  has_many :followers, through: :reverse_of_relationships, source: :follower
 
   has_one_attached :profile_image
 
@@ -27,4 +27,20 @@ class User < ApplicationRecord
   end
   profile_image.variant(resize_to_limit: [width, height]).processed
   end
+
+
+  def follow(user)
+    unless self == user
+      relationships.create(followed_id: user.id)
+    end
+  end
+
+  def unfollow(user)
+    relationships.find_by(followed_id: user.id).destroy
+  end
+
+  def following?(user)
+    followings.inculude?(user)
+  end
+
 end
