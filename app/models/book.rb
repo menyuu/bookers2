177a00b42227ddfa9c3f4836547d1689d1ddea37
menyuu <101458830+menyuu@users.noreply.github.com>
2022-06-08 08:48:01 +0000

    ratyの導入

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,6 +2,11 @@ class Book < ApplicationRecord
 
   validates :title, presence: true
   validates :body, presence: true, length: {maximum: 200}
+  
+  validates :rate, numericality: {
+    less_than_or_equal_to: 5,
+    greater_than_or_equal_to: 1
+  }, presence: true
 
   belongs_to :user
 
