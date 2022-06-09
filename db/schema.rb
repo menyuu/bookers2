b873bff22c4942458a5d9111eb7b8700927e2515
menyuu <101458830+menyuu@users.noreply.github.com>
2022-06-09 06:22:57 +0000

    tag用のモデルを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_08_112533) do
+ActiveRecord::Schema.define(version: 2022_06_09_062006) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -50,6 +50,16 @@ ActiveRecord::Schema.define(version: 2022_06_08_112533) do
     t.index ["user_id"], name: "index_book_comments_on_user_id"
   end
 
+  create_table "book_tags", force: :cascade do |t|
+    t.integer "book_id", null: false
+    t.integer "tag_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["book_id", "tag_id"], name: "index_book_tags_on_book_id_and_tag_id", unique: true
+    t.index ["book_id"], name: "index_book_tags_on_book_id"
+    t.index ["tag_id"], name: "index_book_tags_on_tag_id"
+  end
+
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -75,6 +85,12 @@ ActiveRecord::Schema.define(version: 2022_06_08_112533) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "tags", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -93,6 +109,8 @@ ActiveRecord::Schema.define(version: 2022_06_08_112533) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "book_comments", "books"
   add_foreign_key "book_comments", "users"
+  add_foreign_key "book_tags", "books"
+  add_foreign_key "book_tags", "tags"
   add_foreign_key "favorites", "books"
   add_foreign_key "favorites", "users"
 end
