6bbe1a6e54b01b6320cdf0f4ebf6829d8923ae17
menyuu <101458830+menyuu@users.noreply.github.com>
2022-06-11 14:21:06 +0000

    dotenv-railsの導入

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -83,8 +83,8 @@ Rails.application.configure do
     port:                 587,
     address:              'smtp.gmail.com',
     domain:               'gmail.com',
-    user_name:            '',
-    password:             '',
+    user_name:            ENV['KEY'],
+    password:             ENV['SECRET_KEY']
     authentication:       'login',
     enable_starttls_auto: true
   }
