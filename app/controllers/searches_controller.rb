9b1b853ea243f66f214ecdd1371a98565099ee63
menyuu <101458830+menyuu@users.noreply.github.com>
2022-06-11 09:27:34 +0000

    tagの検索機能の不具合調整

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -13,9 +13,16 @@ class SearchesController < ApplicationController
     unless params[:tag_word]
       @tag = Tag.find(params[:tag_id])
       @books = @tag.books
+      @tag_name = @tag.tag_name
     else
-      @tag = Tag.find_by(tag_name: params[:tag_word])
-      @books = @tag.books
+      unless params[:tag_word] == ""
+        @tag = Tag.find_by(tag_name: params[:tag_word])
+        @books = @tag.books
+        @tag_name = @tag.tag_name
+      else
+        @books = Book.all
+        @tag_name = "All books"
+      end
     end
   end
 
