3be0352dcf4c6487d385b72719a60235c581ed07
menyuu <101458830+menyuu@users.noreply.github.com>
2022-06-12 19:19:46 +0000

    user一覧ページのajax適用

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -4,12 +4,16 @@ class RelationshipsController < ApplicationController
 
   def create
     current_user.follow(params[:user_id])
-    redirect_to request.referer
+    @users = User.all
+    @user = User.find(params[:user_id])
+    # redirect_to request.referer
   end
 
   def destroy
     current_user.unfollow(params[:user_id])
-    redirect_to request.referer
+    @users = User.all
+    @user = User.find(params[:user_id])
+    # redirect_to request.referer
   end
 
   def followings
